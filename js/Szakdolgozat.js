0a6307e60950fc128ce1c2074f8137fd154ec4db
Jakab Zsanett <zsanett28@yahoo.com>
2020-04-25 12:21:16 +0300

    fontos-datumok-visszaszamlalo

--- js/Szakdolgozat.js
+++ js/Szakdolgozat.js
@@ -8,3 +8,51 @@ menuIcon.addEventListener("click", () => {
 function feedback(){
     alert("Sikeres bejelentkezés!");
 }
+
+var dfebr = new Date("Febr 11, 2020 23:59:59").getTime();
+
+var f = setInterval(function () {
+
+    var now = new Date().getTime();
+
+    var time = dfebr - now;
+
+    var days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+    var hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    var minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+
+    var seconds = Math.floor((time % (1000 * 60)) / 1000);
+
+    document.getElementById("leadas-februar").innerHTML = days + " nap " + hours + " óra " + minutes + " perc " + seconds + " másodperc ";
+
+    if(time < 0){
+        clearInterval(dfebr);
+        document.getElementById("leadas-februar").innerHTML = "Lejárt az idő!";
+    }
+}, 1000);
+
+var djun = new Date("Jun 23, 2020 23:59:59").getTime();
+
+var j = setInterval(function () {
+
+    var now = new Date().getTime();
+
+    var time = djun - now;
+
+    var days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+    var hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+
+    var minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+
+    var seconds = Math.floor((time % (1000 * 60)) / 1000);
+
+    document.getElementById("leadas-junius").innerHTML = days + " nap " + hours + " óra " + minutes + " perc " + seconds + " másodperc ";
+
+    if(time < 0){
+        clearInterval(djun);
+        document.getElementById("leadas-junius").innerHTML = "Lejárt az idő!";
+    }
+}, 1000);
